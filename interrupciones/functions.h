1ff413b8e7f30aa2866d24e11e6bdbac3b1a22be
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-20 00:10:45 -0500

    Added functions.h irq

---
+++ interrupciones/functions.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+void NumberUnits(uint64_t number,uint8_t *numVector);
+void PrintNumber(uint8_t number, uint8_t *lcdpins);
+void interrupt_handler();
