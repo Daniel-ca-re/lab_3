1a089df925eed6d56227e4e74728c568641f8ed3
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-20 11:38:41 -0500

    Update main.c

--- interrupciones/main.c
+++ interrupciones/main.c
@@ -9,19 +9,23 @@ uint8_t segment_pins[7] = {12, 11, 19, 20, 21, 10, 22};
 uint8_t display_pins[6] = {16, 17, 18, 15, 14, 13};
 uint8_t frequency_vector[6] = {0, 0, 0, 0, 0, 0};
 
-bool repeating_timer_callback(struct repeating_timer *t) {
+bool repeating_timer_callback1(struct repeating_timer *t) {
 
     static uint8_t display_on = 0; 
-
-    if(display_on==0){
-        NumberUnits(cnt*56, frequency_vector);
-        cnt=0;
-    }
     
     gpio_put(display_pins[display_on++], 0);
     display_on=display_on%6;
     PrintNumber(frequency_vector[display_on], segment_pins);
     gpio_put(display_pins[display_on], 1);
+
+    return true;
+    
+}
+
+bool repeating_timer_callback2(struct repeating_timer *t) {
+
+    NumberUnits(cnt, frequency_vector);
+    cnt=0;
 
     return true;
     
@@ -47,8 +51,11 @@ void main(){
     }
 
     ///timers
-    struct repeating_timer timer;
-    add_repeating_timer_ms(3, repeating_timer_callback, NULL, &timer);
+    struct repeating_timer timer1;
+    add_repeating_timer_ms(3, repeating_timer_callback1, NULL, &timer1);
+
+    struct repeating_timer timer2;
+    add_repeating_timer_ms(1000, repeating_timer_callback2, NULL, &timer2);
 
 
     while(1){
