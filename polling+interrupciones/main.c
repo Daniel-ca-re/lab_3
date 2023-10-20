6203633aa552cc986a3249af15a7c824b63b0132
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-20 13:37:09 -0500

    Update main.c

--- polling+interrupciones/main.c
+++ polling+interrupciones/main.c
@@ -1,18 +1,37 @@
 #include "pico/stdlib.h"
 #include "functions.h"
+#include "hardware/sync.h"
 
 #define signal 1
 
 uint64_t cnt = 0;
 
+bool flagdisp = 0;
+bool flag1seg = 0;
+
+
+bool repeating_timer_callback1(struct repeating_timer *t) {
+
+    flagdisp = 1;
+
+    return true;
+}
+
+bool repeating_timer_callback2(struct repeating_timer *t) {
+
+    flag1seg = 1;
+
+    return true;
+}
+
 void main(){
+
     ///constantes de el display
     uint8_t segment_pins[7] = {12, 11, 19, 20, 21, 10, 22};
     uint8_t display_pins[6] = {16, 17, 18, 15, 14, 13};
     uint8_t frequency_vector[6] = {0, 0, 0, 0, 0, 0};
 
-
-    uint8_t display_on = 0; 
+    uint8_t display_on = 0;
 
     ///inicializacion de los puertos gpio
     gpio_init(signal);
@@ -31,32 +50,38 @@ void main(){
         gpio_set_dir(display_pins[i], GPIO_OUT);
     }
 
+
     ///timers
-    uint64_t disp_clock=time_us_64();
-    uint64_t num_clock=time_us_64();
+    struct repeating_timer timer1;
+    add_repeating_timer_ms(3, repeating_timer_callback1, NULL, &timer1);
 
+    struct repeating_timer timer2;
+    add_repeating_timer_ms(1000, repeating_timer_callback2, NULL, &timer2);
 
-    bool state = gpio_get(signal);
 
     while(1){
 
         ///actualiza los displays
-        if(time_us_64()-disp_clock>3000){
+        if(flagdisp){
 
             gpio_put(display_pins[display_on++], 0);
             display_on=display_on%6;
             PrintNumber(frequency_vector[display_on], segment_pins);
             gpio_put(display_pins[display_on], 1);
-            disp_clock=time_us_64();
-
+            
+            flagdisp = 0;
         }
 
         ///actualiza la frecuencia cada seg
-        if(time_us_64()-num_clock>1000000){
+        if(flag1seg){
             NumberUnits(cnt,frequency_vector);
             cnt = 0;
-            num_clock=time_us_64();
+            
+            flag1seg = 0;
         }
+
+        __wfi();
+
     };
 
 }
