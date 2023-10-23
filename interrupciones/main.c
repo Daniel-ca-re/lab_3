d2846e486cc8aa3835e5ef2a1239a87dca3a8ae1
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-22 19:19:23 -0500

    Update main.c

--- interrupciones/main.c
+++ interrupciones/main.c
@@ -1,14 +1,35 @@
+/**
+ * @file main.c
+ * @brief Programa principal para el control de un display de 7 segmentos y manejo de interrupciones.
+ */
+
 #include "pico/stdlib.h"
 #include "functions.h"
+#include "hardware/sync.h"
 
 #define signal 1
 
+/**
+ * @brief Número de flancos de subida contados.
+ */
 uint64_t cnt = 0;
-///constantes de el display
+
+/**
+ * @brief Constantes de configuración del display de 7 segmentos.
+ */
 uint8_t segment_pins[7] = {12, 11, 19, 20, 21, 10, 22};
 uint8_t display_pins[6] = {16, 17, 18, 15, 14, 13};
 uint8_t frequency_vector[6] = {0, 0, 0, 0, 0, 0};
 
+/**
+ * @brief Callback del temporizador para actualizar el display.
+ *
+ * Esta función se llama periódicamente para actualizar el display de 7 segmentos.
+ * Rota los dígitos que se muestran en el display y actualiza la visualización.
+ *
+ * @param t Puntero al temporizador.
+ * @return Devuelve "true" para indicar que el temporizador debe seguir funcionando.
+ */
 bool repeating_timer_callback1(struct repeating_timer *t) {
 
     static uint8_t display_on = 0; 
@@ -22,6 +43,15 @@ bool repeating_timer_callback1(struct repeating_timer *t) {
     
 }
 
+/**
+ * @brief Callback del temporizador para actualizar la frecuencia.
+ *
+ * Esta función se llama periódicamente para actualizar el vector de frecuencia.
+ * Reinicia el contador de flancos de subida y actualiza el vector de frecuencia.
+ *
+ * @param t Puntero al temporizador.
+ * @return Devuelve "true" para indicar que el temporizador debe seguir funcionando.
+ */
 bool repeating_timer_callback2(struct repeating_timer *t) {
 
     NumberUnits(cnt, frequency_vector);
@@ -33,24 +63,26 @@ bool repeating_timer_callback2(struct repeating_timer *t) {
 
 void main(){
     
-    ///inicializacion de los puertos gpio
+    // Inicialización de los puertos GPIO y configuración de interrupciones
     gpio_init(signal);
     gpio_set_dir(signal, GPIO_IN);
     gpio_set_pulls(signal, false, true);
     gpio_set_irq_enabled_with_callback(signal, GPIO_IRQ_EDGE_RISE, true, &interrupt_handler);
     irq_set_enabled(PIO0_IRQ_0, true);
 
+    // Inicialización de pines para el display de 7 segmentos
     for(int i=0;i<7;i++){
         gpio_init(segment_pins[i]);
         gpio_set_dir(segment_pins[i], GPIO_OUT);
     }
 
+    // Inicialización de pines para el display de 6 dígitos
     for(int i=0;i<6;i++){
         gpio_init(display_pins[i]);
         gpio_set_dir(display_pins[i], GPIO_OUT);
     }
 
-    ///timers
+    // Creación de temporizadores para actualización del display y la frecuencia
     struct repeating_timer timer1;
     add_repeating_timer_ms(3, repeating_timer_callback1, NULL, &timer1);
 
@@ -59,7 +91,8 @@ void main(){
 
 
     while(1){
-
+        // Modo de espera para ahorrar energía
+        __wfi();
     };
 
 }
