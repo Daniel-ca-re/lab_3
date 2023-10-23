234620532d19e00e4ef7032ec0d357273c36e398
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-22 19:11:55 -0500

    Update functions.h

--- polling+interrupciones/functions.h
+++ polling+interrupciones/functions.h
@@ -1,7 +1,26 @@
+/**
+ * @file functions.h
+ * @brief Encabezado de funciones para controlar un display de 7 segmentos y manejar interrupciones.
+ */
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+/**
+ * @brief Esta función toma un número y descompone sus dígitos decimales, es decir, D_{0}*10^0 + D_{1}*10^1 + D_{2}*10^2...
+ * @param number El número D_{2}D_{1}D_{0}.
+ * @param numVector Un vector que almacenará los dígitos decimales del número [D_{2}, D_{1}, D_{0}].
+ */
 void NumberUnits(uint64_t number,uint8_t *numVector);
+
+/**
+ * @brief Esta función enciende los LEDs de un display de 7 segmentos para representar un número.
+ * @param number El número D_{2}D_{1}D_{0}.
+ * @param segment_pins Pines asociados al display de 7 segmentos.
+ */
 void PrintNumber(uint8_t number, uint8_t *lcdpins);
+
+/**
+ * @brief Función de manejo de interrupciones.
+ */
 void interrupt_handler();
