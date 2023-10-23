42e7eebd24b383df6487c9635fabf97940528239
Valentina Jaramillo Raquejo <58915261+valentinajaramillor@users.noreply.github.com>
2023-10-22 19:19:59 -0500

    Update functions.h

--- interrupciones/functions.h
+++ interrupciones/functions.h
@@ -1,7 +1,31 @@
+/**
+ * @file functions.h
+ * @brief Encabezado de funciones para controlar un display de 7 segmentos y manejar interrupciones.
+ */
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+/**
+ * @brief Esta función toma un número y descompone sus dígitos decimales.
+ *
+ * @param number El número D_{2}D_{1}D_{0}.
+ * @param numVector Un vector que almacenará los dígitos decimales del número [D_{2}, D_{1}, D_{0}].
+ */
 void NumberUnits(uint64_t number,uint8_t *numVector);
+
+/**
+ * @brief Esta función enciende los LEDs de un display de 7 segmentos para representar un número.
+ *
+ * @param number El número D_{2}D_{1}D_{0}.
+ * @param segment_pins Pines asociados al display de 7 segmentos.
+ */
 void PrintNumber(uint8_t number, uint8_t *lcdpins);
+
+/**
+ * @brief Manejador de interrupción por flanco de subida.
+ *
+ * Esta función se llama cuando se produce un flanco de subida en la entrada.
+ * Incrementa el contador de flancos de subida.
+ */
 void interrupt_handler();
